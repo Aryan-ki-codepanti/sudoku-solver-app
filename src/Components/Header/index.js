a1cdb0c5ee9a91d46242e051d5cb2a5f31ae5529
Aryan Sethi <aryan.rdps@gmail.com>
2023-01-29 16:08:00 +0530

    Made header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -3,12 +3,29 @@ import "./header.css";
 const Header = () => {
     return (
         <div className="Header">
-            <div className="">left</div>
+            <div className="header-left">
+                <div className="box opacity-30"></div>
+                <div className="box opacity-30"></div>
+                <div className="box opacity-30"></div>
+                <div className="box opacity-10"></div>
+                <div className="box opacity-10"></div>
+                <div className="box opacity-5 last"></div>
+            </div>
 
             <div className="container">
                 <h1>Sudoku</h1>
             </div>
-            <div className="">right</div>
+            <div className="header-right">
+                <div className="box opacity-30"></div>
+                <div className="box opacity-30"></div>
+                <div className="box opacity-30"></div>
+                <div className="box opacity-30"></div>
+                <div className="box opacity-10"></div>
+                <div className="box opacity-10"></div>
+                <div className="box opacity-10"></div>
+                <div className="box opacity-5 last-a"></div>
+                <div className="box opacity-5 last-b"></div>
+            </div>
         </div>
     );
 };
